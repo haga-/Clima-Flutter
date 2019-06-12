86d43043b7b29a09ed1eea01b20042082fe5148a
Heitor <hagalmeida@inf.ufpel.edu.br>
2019-06-12 12:08:23 -0300

    Displaying city weather

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,13 @@ const apiKey = '2e5df4eee26cd4d20fedc374fca4c527';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appId=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
